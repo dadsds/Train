00c255793d42a20a94500d06a07e9f93c6f664f6
dadsds <kuhito@mail.ru>
2016-03-17 23:40:37 +0200

    1
    1

---
+++ Triangle/Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleNamespace
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //string s = Console.ReadLine();
+            //double a = Convert.ToDouble(s);
+            try
+            {
+                Triangle t = new Triangle(5, 12, 6);
+                Console.WriteLine("S = " + t.S);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Triangle/Triangle/Triangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleNamespace
+{
+    public class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+            if (!IsTriangle)
+            {
+                throw new ArgumentException("One or more sides of triangle are invalid");
+            }
+        }
+
+        public double S
+        {
+            get
+            {
+                double p;
+                p = (a + b + c) / 2;
+                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+        }
+
+        private bool IsTriangle
+        {
+            get
+            {
+                return a + b > c && a + c > b && b + c > a;
+            }
+        }
+    }
+}
